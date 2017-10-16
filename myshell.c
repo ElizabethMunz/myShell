8562265e055cca91c369c41def5a99e5d53b93e3
ElizabethMunz <munz@temple.edu>
2017-10-16 18:47:57 -0400

    update 7

--- myshell.c
+++ myshell.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 //function declarations
 char* readline();
@@ -42,6 +44,7 @@ void main(int argc, char*argv[]) {
     char** inputArray = parse(input);
     a = execute(inputArray);
   }
+  //system("clear");
 }
 
 //read user input until user hits enter, then return that input string
@@ -78,13 +81,13 @@ char** parse(char* input) {
 //returns 0 if 'quit' was called, returns nonzero otherwise
 int execute(char** args) {
   int ret = 1; //returns 0 on quit, 1 otherwise
-
   int isBuiltin = 0;
   int isRedirectIn = 0;
   int isRedirectOut = 0;
   int inBg = 0;
   int exitStat, i, j;
-  int stdinDup, stdoutDup;
+  int stdinDup, stdoutDup, infile, outfile;
+  int fd[2];
 
   //return without executing anything if user didn't type anything
   if(args[0] == NULL) {
@@ -98,17 +101,19 @@ int execute(char** args) {
       inBg = 1;
     } 
    if(strcmp(args[i], "<") == 0) {
-      printf("redirect input to %s\n", args[i+1]);
-      stdinDup = dup(STDIN_FILENO);
+      infile = open(args[i+1], O_RDONLY);
+      args[i] = NULL;
       isRedirectIn = 1;
     }
     else if(strcmp(args[i], ">") == 0) {
-      printf("redirect output to %s\n", args[i+1]);
-      FILE* fp = fopen(args[i+1], "w");
-      stdoutDup = dup(STDOUT_FILENO);
+      outfile = open(args[i+1], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+      args[i] = NULL;
+      isRedirectOut = 1;
     } 
     else if(strcmp(args[i], ">>") == 0) {
-      printf("redirect output to %s\n", args[i+1]);
+      outfile = open(args[i+1], O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+      args[i] = NULL;
+      isRedirectOut = 1;
     }
     i++;
   } 
@@ -116,14 +121,24 @@ int execute(char** args) {
   //check for builtin, then run matching fn if it is
   for(j = 0; j < 7; j++) { //j = number of builtin functions there are
     //check if first arg matches a builtin
-    if(strcmp(args[0], builtins[j]) == 0) {
+    if(strcmp(args[0], builtins[j]) == 0) { 
+      //redirect in/out if necessary
+      stdinDup = dup(STDIN_FILENO);
+      stdoutDup = dup(STDOUT_FILENO);
+
+      if(isRedirectIn == 1) {
+        dup2(infile, STDIN_FILENO);
+      }
+      if(isRedirectOut == 1) {
+        dup2(outfile, STDOUT_FILENO);
+      }
 
       //run function and set return value
       ret = (builtinFN[j])(args);
-      //reset STDOUT if we changed it
-      if(isRedirectIn == 1 || isRedirectOut == 1) {
 
-      }
+      //reset stdin/out to defaults
+      dup2(stdoutDup, STDOUT_FILENO);
+      dup2(stdinDup, STDIN_FILENO);
       isBuiltin = 1;
     }
   }
@@ -144,6 +159,17 @@ int execute(char** args) {
     }
     else if(pid == 0) {
       //in child
+
+      //redirect stdin/out if needed
+      if(isRedirectIn == 1) {
+        dup2(infile, STDIN_FILENO);
+        printf("input redirected to %d\n", infile);
+      }
+      if(isRedirectOut == 1) {
+        dup2(outfile, STDOUT_FILENO);
+        printf("output redirected to %d\n", outfile);
+      }
+      //exec the invoked program
       execvp(args[0], args) || 1;
       //there was no file to exec, so tell the user they're wrong
       printf("Command or executable not recognized.\n");
@@ -156,7 +182,8 @@ int execute(char** args) {
       if(inBg == 0) {
         waitpid(pid, &exitStat, 0);
       }
-      return exitStat;
+      printf("execute is returning a value of %d\n", exitStat);
+      //return exitStat;
     }
   }
   return ret;
@@ -164,11 +191,15 @@ int execute(char** args) {
 
 //---------------------builtin functions------------------------------
 
-//quit shell
+/* quit this shell
+*/
 int quit(char** args) {
   return 0;
 }
 
+
+/* change current working directory
+ */
 int cd(char** args) {
   if(chdir(args[1]) != 0) {
     printf("No directory '%s' found.\n", args[1]);
@@ -176,9 +207,10 @@ int cd(char** args) {
   return 1;
 }
 
+/* clear the terminal screen
+*/
 int clear(char** args) {
   //system("clear"); //this function isn't portable, only works in UNIX
-
   //get height of window so we know how many lines to print at a time
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -211,6 +243,8 @@ int dir(char** args) {
   return 1;
 }
 
+/* Print current environment PATH strings
+*/
 int environ(char** args) {
   const char* env = getenv("PATH");
   if(env != NULL) {
@@ -222,6 +256,8 @@ int environ(char** args) {
   return 1;
 }
 
+/* Print [args]
+*/
 int echo(char** args) {
   int i = 1;
   while(args[i] != NULL) {
@@ -232,13 +268,9 @@ int echo(char** args) {
   return 1;
 }
 
-//print out explanation of how to use the shell
+/*print readme file (explanation of how to use the shell)
+*/
 int help(char** args) {
-  //get height of window so we know how many lines to print at a time
-  struct winsize w;
-  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-  int rows = w.ws_row - 1;  
-
   //open readme
   FILE* fp = fopen("readme", "r");
   if(fp == NULL) {
@@ -249,36 +281,53 @@ int help(char** args) {
   fseek(fp, 0, SEEK_END);
   int len = ftell(fp);
   fseek(fp, 0, SEEK_SET);
-
   char text[len];
-  char* input;
-  int r = 1;
-  //print first screen of readme file
-  do {
-    fgets(text, len, fp);
-    printf("%s\n", text);
-    r++;
-  }
-  while(text != NULL && r < rows/2);
-  //now wait for user to hit enter or q to print next line or quit
-  while(1) {
-    input = readline();
-    if(strcmp(input, "q") == 0) {
-      return 1;
+
+  //if current stdout is the terminal, print some of readme file and then wait for user to hit enter to show more
+  if(isatty(1)) {  
+    //get height of window so we know how many lines to print at a time
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int rows = w.ws_row - 1;  
+    
+    char* input;
+    int r = 1;
+    //print first screen of readme file
+    do {
+      fgets(text, len, fp);
+      printf("%s", text);
+      r++;
     }
-    else {
-      if(fgets(text, len, fp) == NULL) {
-        break;
+    while(text != NULL && r < rows);
+    //now wait for user to hit enter to print next line or q to quit
+    while(1) {
+      input = readline();
+      if(strcmp(input, "q") == 0) {
+        return 1;
       }
       else {
-        printf("%s", text);
+        if(fgets(text, len, fp) == NULL) {
+          break;
+        }
+        else {
+          //get rid of trailing newline then print line
+          strtok(text, "\n");
+          printf("%s", text);
+        }
       }
+    }
+
+    printf("Reached end of help file. Press enter to return to the shell.\n");
+    //wait for any key press & enter to exit the function
+    input = readline();
+  }
+  //STDOUT isn't the terminal, so just print the whole help file and don't wait for any user input
+  else {
+    while(fgets(text, len, fp) != NULL ) {
+      printf("%s", text);
     }
   }
 
-  printf("Reached end of help file. Press enter to return to the shell.\n");
-  //wait for any key press & enter to exit the function
-  input = readline();
   return 1;
 }
 
