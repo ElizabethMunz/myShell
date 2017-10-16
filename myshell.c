b57887e8e30cd2bb0f015204e0965c6c2d8ad7d3
ElizabethMunz <munz@temple.edu>
2017-10-16 18:43:39 -0400

    began implementation of dir & help commands

--- myshell.c
+++ myshell.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
 
 //function declarations
 char* readline();
@@ -9,11 +11,11 @@ char** parse(char* input);
 int execute(char** args);
 //builtin functions
 int quit(char** args);
-int cd(char* directory);
+int cd(char** args);
 int clear(char** args);
-int dir(char* directory);
+int dir(char** args);
 int environ(char** args);
-int echo(char* comment);
+int echo(char** args);
 int help(char** args);
 
 #define BUFSIZE  1024
@@ -26,10 +28,12 @@ int (*builtinFN[])(char **) = {&quit, &cd, &clear, &dir, &environ, &echo, &help}
 void main(int argc, char*argv[]) {
   int a = 1;
   while(a == 1) {
-    printf("myshell>");
+    //get current directory to print in prompt
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    //print prompt
+    printf("%s/myShell>", cwd);
     char* input = readline();
-    //this printf is just for testing purposes
-    //printf("%s\n", input);
     char** inputArray = parse(input);
     a = execute(inputArray);
   }
@@ -106,15 +110,15 @@ int quit(char** args) {
   return 0;
 }
 
-int cd(char* directory) {
-  if(chdir(directory) != 0) {
-    printf("No directory '%s' found.\n", directory);
+int cd(char** args) {
+  if(chdir(args[1]) != 0) {
+    printf("No directory '%s' found.\n", args[1]);
   }
   return 1;
 }
 
 int clear(char** args) {
-  int screenHeight = 50;
+  int screenHeight = 70; //hardcode a number for screenHeight b/c there's no portable way to get actual window height in C
   int i;
   for(i = 0; i < screenHeight; i++) {
     printf("\n");
@@ -122,8 +126,25 @@ int clear(char** args) {
   return 1;
 }
 
-int dir(char* directory) {
-  printf("dir command executed\n");
+int dir(char** args) {
+  DIR *dir;
+  struct dirent *dirContents;
+  if(args[1] == NULL) {
+    //if no argument specified, get current directory contents
+    dir = opendir("./");
+  }
+  else {
+    //check if specified directory exists, open it
+    if((dir = opendir(args[1])) == NULL) {
+      printf("No such directory %s.\n", args[1]);
+      return 1;
+    }
+  }
+  while((dirContents = readdir(dir)) != NULL) {
+    printf(" %s ", dirContents->d_name);
+  }
+  printf("\n");
+  closedir(dir);
   return 1;
 }
 
@@ -132,14 +153,28 @@ int environ(char** args) {
   return 1;
 }
 
-int echo(char* comment) {
-  printf("%s\n", comment);
+int echo(char** args) {
+  printf("%s\n", args[1]);
   return 1;
 }
 
 //print out explanation of how to use the shell
 int help(char** args) {
-  printf("myShell HELP\n");
+  printf("----------MYSHELL HELP----------\n How to use:  help [command]  to learn about built-in command\n--------------------------------\n");
+  if(args[1] == NULL) {
+    printf("Commands:\n cd\n clear\n dir\n echo\n environ\n help\n quit\n");
+  }
+  else {
+    //give help for specific command
+    int j;
+    for(j = 0; j < 7; j++) { //j = number of builtin functions there are
+      if(strcmp(args[1], builtins[j]) == 0) {
+        printf("How to use: %s\n", builtins[j]);
+        break;
+      }
+    }
+    
+  }
   return 1;
 }
 
