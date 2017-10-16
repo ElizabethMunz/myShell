2d9bb3abf031446b06818f6271cba5524e287b78
ElizabethMunz <munz@temple.edu>
2017-10-16 18:46:46 -0400

    version 5

--- myshell.c
+++ myshell.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
+#include <signal.h>
 
 //function declarations
 char* readline();
@@ -24,8 +26,10 @@ int help(char** args);
 char* builtins[] = {"quit", "cd", "clear", "dir", "environ", "echo", "help"};
 int (*builtinFN[])(char **) = {&quit, &cd, &clear, &dir, &environ, &echo, &help};
 
-// The main function: infinite loop that is the shell
+// The main function: loop that is the shell prompt
 void main(int argc, char*argv[]) {
+  //clear screen when entering shell
+  system("clear");
   //a will be the exit code/return value of any executed program or function
   int a = 1;
   while(a != 0) {
@@ -73,52 +77,93 @@ char** parse(char* input) {
 }
 
 //based on values of args array, execute appropriate function (either builtin or external)
+//returns 0 if 'quit' was called, returns nonzero otherwise
 int execute(char** args) {
   int ret = 1; //returns 0 on quit, 1 otherwise
+
   int isBuiltin = 0;
-  int exitStat;
+  int isRedirect = 0;
+  int inBg = 0;
+ 
+  int exitStat, i, j;
 
-  //create a pipe
-  int fd[2];
-  if(pipe(fd) == -1) {
-    printf("Pipe error\n");
-  }
-
-  //FORK to create child
-  pid_t pid;
-  pid = fork();
-  if(pid < 0) {
-    printf("Error forking\n");
-  }
-  else if(pid == 0) {
-    //in child
-    //TODO: check if we need to redirect I/0 or run in background
-    
-
-    //check for builtin, then run matching fn if it is
-    int j;
-    for(j = 0; j < 7; j++) { //j = number of builtin functions there are
-      if(strcmp(args[0], builtins[j]) == 0) {
-        //set return value
-        ret = (builtinFN[j])(args);
-        isBuiltin = 1;
-      }
+  //check if we need to run in background   
+  i = 0;
+  while(args[i] != NULL) {
+    if(strcmp(args[i], "&") == 0) {
+      inBg = 1;
+    } 
+ /*  if(strcmp(args[i], "<") == 0) {
+      printf("redirect input to %s\n", args[i+1]);
     }
-    //if we didn't find a builtin fn, try to exec a file
-    if(isBuiltin == 0) {
+    else if(strcmp(args[i], ">") == 0) {
+      printf("redirect output to %s\n", args[i+1]);
+    } 
+    else if(strcmp(args[i], ">>") == 0) {
+      printf("redirect output to %s\n", args[i+1]);
+    } */
+    i++;
+  } 
+
+  //check for builtin, then run matching fn if it is
+  for(j = 0; j < 7; j++) { //j = number of builtin functions there are
+    //check if first arg matches a builtin
+    if(strcmp(args[0], builtins[j]) == 0) {
+      //check if output needs redirecting
+      i = 0;
+      while(args[i] != NULL) {
+        if(strcmp(args[i], ">") == 0) {
+          //dupe then dup2
+          isRedirect = 1;
+        }
+        else if(strcmp(args[i], ">>") == 0) {
+          //dup then dup2
+          isRedirect = 1;
+        }
+        i++;
+      }    
+      //run function and set return value
+      ret = (builtinFN[j])(args);
+      //reset STDOUT if we changed it
+      if(isRedirect == 1) {
+
+      }
+      isBuiltin = 1;
+    }
+  }
+
+  //if we didn't find a builtin fn, fork and try to exec a file 
+  if(isBuiltin == 0) {
+    //create a pipe
+    int fd[2];
+    if(pipe(fd) == -1) {
+      printf("Pipe error\n");
+    }
+
+    //FORK to create child
+    pid_t pid;
+    pid = fork();
+    if(pid < 0) {
+      printf("Error forking\n");
+    }
+    else if(pid == 0) {
+      //in child
       execvp(args[0], args) || 1;
       //there was no file to exec, so tell the user they're wrong
       printf("Command or executable not recognized.\n");
+      //exit with return value
+      exit(ret);
     }
-    //exit with return value
-    exit(ret);
+    else {
+      //in parent
+      //wait for child to finish if we aren't running in bg
+      if(inBg == 0) {
+        waitpid(pid, &exitStat, 0);
+      }
+      return exitStat;
+    }
   }
-  else {
-    //in parent
-    waitpid(pid, &exitStat, 0);
-    //printf("exit status of child: %d\n", exitStat);
-    return exitStat;
-  }
+  return ret;
 }
 
 //---------------------builtin functions------------------------------
@@ -129,20 +174,20 @@ int quit(char** args) {
 }
 
 int cd(char** args) {
-  printf("cd executed, changing to %s\n", args[1]);
   if(chdir(args[1]) != 0) {
     printf("No directory '%s' found.\n", args[1]);
   }
-  char cwd[1024];
-  getcwd(cwd, sizeof(cwd));
-  printf("current working directory: %s\n", cwd);
   return 1;
 }
 
 int clear(char** args) {
-  int screenHeight = 100; //hardcode a number for screenHeight b/c there's no portable way to get actual window height in C
-  int i;
-  for(i = 0; i < screenHeight; i++) {
+  //system("clear"); //this function isn't portable, only works in UNIX
+
+  //get height of window so we know how many lines to print at a time
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  int r;
+  for(r = 0; r < w.ws_row; r++) {
     printf("\n");
   }
   return 1;
@@ -193,22 +238,67 @@ int echo(char** args) {
 
 //print out explanation of how to use the shell
 int help(char** args) {
-  printf("----------MYSHELL HELP----------\n How to use:  help [command]  to learn about built-in command\n--------------------------------\n");
-  if(args[1] == NULL) {
-    printf(" Commands:\n  cd\n  clear\n  dir\n  echo\n  environ\n  help\n  quit\n");
-    printf(" This shell also accepts running executable files. Type the relative path of the executable and hit enter to run it.\n");
+  //get height of window so we know how many lines to print at a time
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  int rows = w.ws_row - 1;  
+
+  //open readme
+  FILE* fp = fopen("readme", "r");
+  //get length of readme file so array can be large enough to hold it
+  fseek(fp, 0, SEEK_END);
+  int len = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+
+  char text[len];
+  char* input;
+  int r = 1;
+  //print first screen of readme file
+  do {
+    fgets(text, len, fp);
+    printf("%s", text);
+    r++;
   }
-  else {
-    //give help for specific command
-    int j;
-    for(j = 0; j < 7; j++) { //j = number of builtin functions there are
-      if(strcmp(args[1], builtins[j]) == 0) {
-        printf(" How to use: %s\n", builtins[j]);
+  while(text != NULL && r < rows);
+  //now wait for user to hit enter or q to print next line or quit
+  while(1) {
+    input = readline();
+    if(strcmp(input, "q") == 0) {
+      return 1;
+    }
+    else {
+      if(fgets(text, len, fp) == NULL) {
         break;
+      }
+      else {
+        printf("%s", text);
       }
     }
   }
-  printf("--------------------------------\n");
+
+/*  while(text != NULL) {
+    //print the first screen-ful of help page
+    printf("%s", text);
+    while(r < rows) {
+      fgets(text, len, fp);
+      printf("%s", text);
+      r++;
+    }
+    //wait for enter cmd to read next line or q to quit
+    input = readline();
+    if(strcmp(input, "q") == 0) {
+      return 1;
+    }
+    else {
+      if(fgets(text, len, fp) == NULL) {
+        break;
+      }
+      printf("%s", text);
+    }
+  }*/
+  printf("Reached end of help file. Press enter to return to the shell.\n");
+  //wait for any key press & enter to exit the function
+  input = readline();
   return 1;
 }
 
